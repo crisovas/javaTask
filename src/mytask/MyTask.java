cbab812b944b8bc34dfec932045f1ebb9c716969
Chris Samuel <Chris Samuel@DESKTOP-U0V2R58>
2020-06-06 11:32:05 +0100

    this is my task

---
+++ src/mytask/MyTask.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytask;
+
+public class MyTask {
+
+    public static void main(String[] args) {
+        String firstName;
+        String lastName;
+        String language;
+        
+          
+          firstName = "[Christian]";
+          lastName = "[Samuel]";
+          language = "[Java]";
+          
+          System.out.print("Hello World, this is");
+          System.out.print(" "+ firstName);
+          System.out.println(" "+ lastName);
+          System.out.println("With HNGi7 [06727]");
+          System.out.println("Using " + language + " for stage 2 task ");
+          System.out.println("crismiles@gmail.com");
+        
+    }
+    
+}
